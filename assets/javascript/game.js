d091afff504cde5b5954bd2d5b000bcecc477fda
Max Wilets <mwilet@gmail.com>
2018-09-17 15:48:36 -0600

    unit4game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,34 +3,10 @@
 
 
 $(document).ready(function() {
-    var sephCard = {
-        name: "sephiroth",
-        hp: 180,
-        counter: 30,
-        atk: 14
-    };
-    
-    var vinCard = {
-        name: "vincent",
-        hp: 140,
-        counter: 20,
-        atk: 10
-    };
-    var lightCard = {
-        name: "lightning",
-        hp: 120,
-        counter: 15,
-        atk: 8
-    }
-    var beaCard = {
-        name: "beatrix",
-        hp: 160,
-        counter: 25,
-        atk: 12
-    };
+
     attacker= true
 
-    var charactersAr = [sephCard, lightCard, beaCard, vinCard];
+  
 
     var seph = $(".sephiroth");
 // console.log(seph); 
@@ -44,56 +20,111 @@ var bea = $(".beatrix");
 
 var vin = $(".vincent");
 // console.log(vin);
-
+//setting an array for the characters
 var heroes = [seph, light, bea, vin];
 var enemy;
-
-
-
+var damage = 0;
+var defeated = false
      
          $(this).on("click", ".hero", function(event) {
     console.log(event.currentTarget.attributes.value.value);
+   //pulls value from html this value is equal to the selected
+   //array index
     var cardValue = event.currentTarget.attributes.value.value;
+    hhp = parseInt(event.currentTarget.attributes.hp.value)
+    attack= parseInt(event.currentTarget.attributes.atk.value);
+    hero = this
+    console.log(hhp);
+   
+    console.log(attack);
     // heroes = [seph, light, bea, vin];
 
-    $(".character").after(this);
+    $(".character").after(hero);
+    //removes the selected card form the array
     heroes.splice(parseInt(cardValue),1);
     
-
+    
+    
     attacker = false;
     
     // console.log(defenders);
-    
+    //buts the new array in the attack1 div
     $(".attackl").append(heroes);
 
     //  console.log($(".def"));
     for (var i=0; i <= heroes.length; i++) {
         heroes[i].removeClass("hero");
         heroes[i].addClass("def");
-        console.log(heroes.class)
+         
      }
-    attack= beaCard.atk;
-    damage = attack + beaCard.atk;})
+    
+    })
     $(this).on("click", ".def", function (event) {
         // console.log(this);
        $(".enemy").after(this);
+       badGuy = (this);
+     
+ 
+        badHp = parseInt(event.currentTarget.attributes.hp.value);
+       badName = (event.currentTarget.attributes.name.value);
+       badCounter= (event.currentTarget.attributes.counter.value);
+       console.log($(badGuy));
+       console.log(badHp);
+       console.log(badCounter);
+     
+       
+       
       // enemys = event.currentTarget.attribues.value.value;
       // heroes.splice(parseInt(enemys),1);
         // console.log(enemy);
-       
+     //  show = event.currentTarget.attribues.value.data;
+     //  console.log(show);
+        
       
        $("button").on("click", function() {
-           
-        console.log("bad guy")
-       //  fight = true;
+        console.log(badGuy);
+        badCHP = badHp;
+       
+       var fight = true;
        //  damage = attack + .atk;
- 
-          console.log(damage);
-         if (fight === true) {
-             attack = attack + beaCard.atk;
-             fight = false;
+          if (fight) {
+            //damage the character does  
+            
+            damage = damage + attack
+              
+              console.log(damage);
+              //remaining hp for enemy
+              badHp = badHp - damage;
+             //remaining hp for hero
+             hhp = hhp -badCounter;
+             console.log("the character has" + hhp + "left");
+             defeated = true;
+              console.log(badHp);
+              
+              
+                
+
+          };
+          fight = false;
+          if (badHp <= 0) {
+              alert("Congratulations you have defeated " + badName + "! Choose a new opponent");
+              badGuy.remove();
+              delete badHp;
+    
+              delete badCounter;
+              
+              delete badGuy;
+
+              delete badName;
+              
+          }
+          
+          
+        // if (fight === true) {
+          //   attack = attack + beaCard.atk;
+            // fight = false;
              
-         };
+        // };
     })
         
     })
