06819d7cec60549abba5f8f74151002d0eee8aef
Max Wilets <mwilet@gmail.com>
2018-09-17 21:50:51 -0600

    unit4game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,9 @@
 
 
 
-$(document).ready(function() {
-
+$(document).ready(function() 
+{
+alert("Welcome to my Final Fantasy Game Choose a Character")
     attacker= true
 
   
@@ -21,19 +22,22 @@ var bea = $(".beatrix");
 var vin = $(".vincent");
 // console.log(vin);
 //setting an array for the characters
-var heroes = [seph, light, bea, vin];
-var enemy;
+var heroes = [
+    seph, light, bea, vin
+];
+
 var damage = 0;
-var defeated = false
+
      
          $(this).on("click", ".hero", function(event) {
-    console.log(event.currentTarget.attributes.value.value);
+    
    //pulls value from html this value is equal to the selected
    //array index
+  
     var cardValue = event.currentTarget.attributes.value.value;
     hhp = parseInt(event.currentTarget.attributes.hp.value)
     attack= parseInt(event.currentTarget.attributes.atk.value);
-    hero = this
+    hero = $(this);
     console.log(hhp);
    
     console.log(attack);
@@ -43,36 +47,59 @@ var defeated = false
     //removes the selected card form the array
     heroes.splice(parseInt(cardValue),1);
     
-    
-    
+
+   
     attacker = false;
     
     // console.log(defenders);
     //buts the new array in the attack1 div
-    $(".attackl").append(heroes);
-
+    $(".attackl").html(heroes);
+     alert("Now Choose an Enemy");
     //  console.log($(".def"));
-    for (var i=0; i <= heroes.length; i++) {
+    for (var i=0; i <= heroes.length; i++) 
+    {
         heroes[i].removeClass("hero");
         heroes[i].addClass("def");
          
      }
     
+    
     })
-    $(this).on("click", ".def", function (event) {
+    $(this).on("click", ".def", function (event)
+     {
         // console.log(this);
-       $(".enemy").after(this);
-       badGuy = (this);
-     
- 
-        badHp = parseInt(event.currentTarget.attributes.hp.value);
-       badName = (event.currentTarget.attributes.name.value);
-       badCounter= (event.currentTarget.attributes.counter.value);
-       console.log($(badGuy));
-       console.log(badHp);
-       console.log(badCounter);
-     
+        badGuy = $(this);
+        VBadGuy= [];
        
+    
+       //Stats for the enemy
+       badHp = parseInt(event.currentTarget.attributes.hp.value);
+       badName = (event.currentTarget.attributes.name.value);
+       badCounter= parseInt(event.currentTarget.attributes.counter.value);
+       
+
+
+      (VBadGuy).push(badGuy, badHp, badName, badCounter);
+        //put variables in an array
+       //move the rest of the characters on a row 
+       $(".enemy").html($(badGuy));
+       $(badGuy).addClass("opponent");
+       
+       //setting variables to span into where the html is on the card to live update remaining HP
+       var look = (badGuy.children($()));
+       
+       var look2 = (look.children($("div.card-body")));
+       
+       var look3 = (look2.children($("span").value));
+       
+
+       var heroH = $((hero).children($()));
+    
+       var heroH2 = ($(heroH).children($("div.card-body")));
+     
+       var heroH3 = ($(heroH2).children($("span").value));
+      
+
        
       // enemys = event.currentTarget.attribues.value.value;
       // heroes.splice(parseInt(enemys),1);
@@ -81,41 +108,45 @@ var defeated = false
      //  console.log(show);
         
       
-       $("button").on("click", function() {
-        console.log(badGuy);
-        badCHP = badHp;
-       
+       $("button").on("click", function() 
+       {
+        
+      
        var fight = true;
        //  damage = attack + .atk;
           if (fight) {
             //damage the character does  
-            
+       
             damage = damage + attack
               
               console.log(damage);
               //remaining hp for enemy
-              badHp = badHp - damage;
+              VBadGuy[1] = VBadGuy[1] - damage;
+              console.log(VBadGuy[1])
              //remaining hp for hero
-             hhp = hhp -badCounter;
-             console.log("the character has" + hhp + "left");
+             hhp = hhp - VBadGuy[3];
+            
              defeated = true;
-              console.log(badHp);
+             $(look3).html("<h5>HP: " + VBadGuy[1] + "</h5>");
+             $(heroH3).html("<h5> HP: " + hhp + "</h5");
               
               
-                
-
-          };
+              };
+          if (hhp <= 0) {
+              alert("Your hero has fallen, thus is the futility of war! Try again");
+              location.reload(true);
+              //if your character runs out of hp it resets the game
+          }
           fight = false;
-          if (badHp <= 0) {
-              alert("Congratulations you have defeated " + badName + "! Choose a new opponent");
-              badGuy.remove();
-              delete badHp;
-    
-              delete badCounter;
+          if (VBadGuy[1] <= 0) {
+              alert("Congratulations you have defeated " + badName + "! But even mountains turn to sand, Choose a new opponent");
+            //  $(badGuy).removeClass(".opponent")
+             // $(badGuy).remove();
+              delete VBadGuy;
+              $(".enemy").empty();
               
-              delete badGuy;
-
-              delete badName;
+              console.log($(".enemy"));
+    
               
           }
           
